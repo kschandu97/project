8d9ff2f937f78817b24810f68f60d43daeb1a7c4
kschandu97 <52074031+kschandu97@users.noreply.github.com>
2019-07-18 15:03:53 +0530

    Add files via upload

---
+++ app/app.addDetails.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+@Component({
+    selector:'add-det',
+    templateUrl:'app.addDetails.html'
+})
+ export class AddDetailsComponent{ 
+    empName:string;
+    empAddress:string;
+    empPin:number;
+ }
+   
